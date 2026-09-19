ee612470584344d99d95d654783074804a3f36f1
Tamara Ershova <tamara.ershova2010@yandex.ru>
2026-09-19 15:52:55 +0300

    Выполнены базовые задания 1-5

---
+++ task_3.py
@@ -0,0 +1,23 @@
+world_champions = {
+    2002: 'Бразилия',
+    2006: 'Италия',
+    2010: 'Испания',
+    2014: 'Германия',
+    2018: 'Франция',
+}
+
+world_champions[2022] ='Аргентина'
+
+world_champions_keys = list(world_champions.keys())
+
+for year in world_champions_keys:
+    print(year,'-',world_champions[year])
+
+country = 'Италия'
+
+if country in world_champions.values():
+    print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
+else:
+    print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
+        
+          
